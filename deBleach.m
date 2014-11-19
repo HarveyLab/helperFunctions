c65059637c64ff7c542e5590627844d808fa7848
mjlm <m.minderer@gmail.com>
2014-11-19 18:36:53 -0500

    Added deBleaching function.
    deBleach removes trends from long fluorescence traces.

---
+++ deBleach.m
@@ -0,0 +1,17 @@
+function f = deBleach(f, mode)
+% f = deBleach(f, mode) removes bleaching 
+
+switch mode
+    case 'exponential'
+        % Robustly fit a straight line to log(fluorescence) and then
+        % subtract exp(straightLine).
+        f(f<0.1) = 0.1; % So that log() works without imaginary issues.
+        fl = log(f);
+        x = 1:numel(f);
+        b = robustfit(x, fl);
+        f_ = exp(b(1)+b(2)*x);
+        f = f-f_+mean(f_);
+        
+    case 'linear'
+        f = detrend(f);
+end
