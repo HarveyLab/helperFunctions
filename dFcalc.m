aace3a34da2ee95a66d420d3a2071f347aedcdc3
Selmaan <chettih@fas.harvard.edu>
2016-05-08 16:22:49 -0400

    Added dF extraction for discontinuous acquisitions
    In absence of argument, assumes trace is from one continuous
    acquisition. Argument 'acqBlocks' is a matrix of size nBlocksx2, where
    the two columns are the starting and ending frame of each block

--- dFcalc.m
+++ dFcalc.m
@@ -1,18 +1,25 @@
-function dF = dFcalc(subTrace,rawTrace,mode)
+function dF = dFcalc(subTrace,rawTrace,mode, acqBlocks)
 
 if ~exist('mode','var') || isempty(mode)
     mode = 'custom_wfun';
 end
 
+if ~exist('acqBlocks','var') || isempty(acqBlocks)
+    acqBlocks = [1, size(subTrace,2)];
+end
+
+nBlocks = size(acqBlocks,1);
 nSigs = size(subTrace,1);
-dF = subTrace;
-parfor nSig = 1:nSigs
+dF = nan(size(subTrace));
+for nSig = 1:nSigs
     
     if all(isnan(subTrace(nSig,:))) || all(isnan(rawTrace(nSig,:)))
         dF(nSig,:) = nan;
         continue
     end
-    disp(nSig)
-    dF(nSig,:) = (subTrace(nSig,:) - getF_(subTrace(nSig,:), mode))...
-        ./getF_(rawTrace(nSig,:), mode);
+    for nBlock = 1:nBlocks
+        blockInd = acqBlocks(nBlock,1):acqBlocks(nBlock,2);
+        dF(nSig,blockInd) = (subTrace(nSig,blockInd) - getF_(subTrace(nSig,blockInd), mode))...
+            ./getF_(rawTrace(nSig,blockInd), mode);
+    end
 end
